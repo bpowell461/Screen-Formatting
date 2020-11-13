3b892bad6665d5f1a9aa0265e7be6f2fd39a1921
Brad Powell <bpowell461@gmail.com>
2020-11-13 00:25:07 -0500

    Added comments

--- src/Lisp/antiquity.lisp
+++ src/Lisp/antiquity.lisp
@@ -12,7 +12,7 @@
 
 (setq filename (nth 1 sb-ext:*posix-argv*))
 (setq longList (make-array 0 :adjustable t :fill-pointer 0))
-(setq shortList (make-array 10 :adjustable t :fill-pointer 0))
+(setq shortList (make-array 10 :adjustable t :fill-pointer 0)) ;Lisp strings are represented as vectors of characters
 (setq tmpList (make-array 0 :adjustable t :fill-pointer 0))
 (loop for i from 0 to 10
 do (vector-push "aa" shortList)
@@ -21,13 +21,13 @@ do (vector-push "aa" shortList)
 (defun tokenize (string)
   (loop
      for start = 0 then (+ space 1)
-     for space = (position #\space string :start start)
+     for space = (position #\space string :start start)  ;This tokenizes a file by whitespace and returns a list;
      for token = (subseq string start space)
      unless (string= token "") collect token
      until (not space)))
 
 (defparameter *stringList*
-   (with-open-file (stream filename :direction :input)
+   (with-open-file (stream filename :direction :input) ;Returning list
      (loop for line = (read-line stream nil)
            while line
            append (tokenize line))))
@@ -35,7 +35,7 @@ do (vector-push "aa" shortList)
 (format t "~8,d" lineIndex)
 (format t "~d" "  ")
 (loop for token in *stringList* do
-(setq b (ignore-errors (parse-number token)))
+(setq b (ignore-errors (parse-number token))) ;Suppose to check if string is numeric and remove it
 (if (not b)
    (progn
      (if (>  (length token) num)
@@ -56,7 +56,7 @@ do (vector-push "aa" shortList)
         )
      )
 
-     (setq tmpList (make-array 0 :adjustable t :fill-pointer 0))
+     (setq tmpList (make-array 0 :adjustable t :fill-pointer 0)) ;Emptying out vector
      (terpri)
      (incf lineIndex 1)
      (format t "~8,d" lineIndex)
@@ -75,7 +75,7 @@ do (vector-push "aa" shortList)
      (decf num (length token))
      (decf num 1)
 
-     (vector-push-extend token tmpList)
+     (vector-push-extend token tmpList) ;Appending to string
 
      )
 
